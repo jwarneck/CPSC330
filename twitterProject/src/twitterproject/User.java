6c3c59ad77147c0694ad8c2bd8f0636d9a04df98
jwarneck <jordanwarnecke@yahoo.com>
2014-11-05 12:29:46 -0500

    Update of code

---
+++ twitterProject/src/twitterproject/User.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package twitterproject;
+
+/**
+ *
+ * @author jordanwarnecke
+ */
+
+
+public class User {
+    
+    private String userName;
+    
+    public String getUsername(){
+        
+        return userName;
+    }
+    
+    
+    /**
+     * sets user name when account is created
+     * then checks database of usernames to ensure
+     * that name isn't already in use.
+     */
+    public void setUsername(){
+        
+        
+        
+    }
+}
