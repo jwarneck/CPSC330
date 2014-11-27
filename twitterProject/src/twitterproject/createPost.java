f0d657bd7e1d1063690011e54c28f1dc1bf31379
ashran32 <jtcwow@hotmail.com>
2014-11-26 21:18:17 -0500

    work on createPost

--- twitterProject/src/twitterproject/createPost.java
+++ twitterProject/src/twitterproject/createPost.java
@@ -6,6 +6,11 @@
 
 package twitterproject;
 
+import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 /**
  *
  * @author jordanwarnecke
@@ -13,27 +18,93 @@ package twitterproject;
 public class createPost {
     
     String author;
-    int datePosted;
-    String messsage;
+    String datePosted;
+    String message;
+    String messageShown;
     int charCount;
     int numFav;
     int numRP;
     String hashtag;
     String otherUsers;
     
+    Scanner input = new Scanner(System.in);
+    
+    
+    public void setMessage(){
+        System.out.println("Type your message, or 1 to cancel. message must be under 140 characters: ");
+        message = input.nextLine();
+        cancelPost();
+        charLimit();
+        if (message =="1") {
+        	cancelPost();
+        }
+        postMessage();
+        
+        
+    }
+    
     public void postMessage(){
-        //user posts a message
+    	//author = account.getUsername();
+    	DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+        Calendar cal = Calendar.getInstance();
+        datePosted = dateFormat.format(cal.getTime());
+        
+        
+        
+    	message = "Author: " + author + " : " + message + " [Favourited: 0]" + "[Reposted: 0]";
+
+    	try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("C:/Users/User/Desktop/dataFile.txt", true)))) {
+    	    out.println(message);
+    	}catch (IOException e) {
+        	}
+    	
+    	
+    	
+        
     }
     
     public void cancelPost(){
-        //user can cancel posting
+        //Login newLogin = new Login();
+        //newLogin.loginMenu();
     }
     
-    public boolean charLimit(){
+    public void charLimit(){
+    	charCount = message.length();
         if (charCount > 150){
-            return false;
+            setMessage();
         }
-        else
-            return true;
     }
-}
+    public void findPost(){
+    	    String filePath = "C:/Users/User/Desktop/dataFile.txt";
+    	    BufferedReader br;
+    	    String inputSearch = "Author:";
+    	    String line = "";
+
+    	    try {
+    	        br = new BufferedReader(new FileReader(filePath));
+    	        try {
+    	            while((line = br.readLine()) != null)
+    	            {
+    	                //System.out.println(line);
+    	                String[] words = line.split(" ");
+
+    	                for (String word : words) {
+    	                  if (word.equals(inputSearch)) {
+    	                	  System.out.println(word + " ");
+    	                  }
+    	                }
+
+    	            }
+    	            br.close();
+    	        } catch (IOException e) {
+    	            
+    	            e.printStackTrace();
+    	        }
+    	    } catch (FileNotFoundException e) {
+    	        
+    	        e.printStackTrace();
+    	    }
+
+    	}
+
+    }
