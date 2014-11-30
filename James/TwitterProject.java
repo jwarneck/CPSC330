05730fa66e13400a26f6603229774820c96287ba
ashran32 <jtcwow@hotmail.com>
2014-11-29 21:30:28 -0500

    see previous changes for what's in this

---
+++ James/TwitterProject.java
@@ -0,0 +1,25 @@
+package twitterproject;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author jordanwarnecke
+ */
+
+
+
+public class TwitterProject {
+    
+    
+
+    /**
+     * @param args the command line arguments
+     * @throws IOException 
+     */
+    public static void main(String[] args) throws IOException {
+        Login1.Login();
+    	//Menu.Menu();
+    }
+    
+}
